0d6e09c388a79f34a3c02af5b394945525553f57
suim <suim9770@gmail.com>
2023-02-08 19:54:32 +0900

    [10816] (Python) 숫자 카드 2

--- python/sorting/test_simple.py
+++ python/sorting/test_simple.py
@@ -66,6 +66,7 @@ def test_number_1931():
             result += 1
     print(result)
 
+
 # def test_number_10989():
 #     # N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
 #
@@ -80,3 +81,22 @@ def test_number_1931():
 #         number_list.append(int(example_list.popleft()))
 #
 #     print(sorted(number_list))
+
+def test_number_10816():
+    # 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다.
+    # 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
+
+    # 첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다.
+    # 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+    # 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다.
+    # 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+
+    example_list = deque(['10', '6 3 2 10 10 10 -10 -10 7 3', '8', '10 9 -5 2 3 4 5 -10'])
+
+    from collections import Counter
+
+    example_list.popleft()
+    n_dict = Counter(example_list.popleft().split(' '))
+    m_list = [0 for i in range(int(example_list.popleft()))]
+    m_list = example_list.popleft().split(' ')
+    print(' '.join(map(lambda x: str(n_dict[x]) if x in n_dict else '0', m_list)))
