b517a3ee0ec7b79720c67645cdc9270e44d23135
suim <suim9770@gmail.com>
2023-02-09 21:59:18 +0900

    [11650] (Python) 좌표 정렬하기

--- python/sorting/test_simple.py
+++ python/sorting/test_simple.py
@@ -76,11 +76,53 @@ def test_number_1931():
 #
 #     example_list = deque(['10', '5', '2', '3', '1', '4', '2', '3', '5', '1', '7'])
 #
-#     number_list = []
-#     for i in range(int(example_list.popleft())):
-#         number_list.append(int(example_list.popleft()))
+#     class Node:
+#         def __init__(self, data, next_node=None):
+#             self.data = data
+#             self.next_node = None
 #
-#     print(sorted(number_list))
+#     class LinkedList:
+#         def __init__(self, head):
+#             self.head = Node(head)
+#
+#         def first_insert(self, new_node):
+#             new_node.next_node = self.head
+#             self.head = new_node
+#
+#         def insert(self, cur_node, new_node):
+#             new_node.next_node = cur_node.next_node
+#             cur_node.next_node = new_node
+#
+#         def show(self):
+#             cur_node = self.head
+#             while cur_node.next_node is not None:
+#                 print(cur_node.data)
+#                 cur_node = cur_node.next_node
+#             print(cur_node.data)
+#
+#     max_len = int(example_list.popleft())
+#     linked_list = LinkedList(int(example_list.popleft()))
+#
+#     for i in range(max_len - 1):
+#         cur_node = linked_list.head
+#         new_node = Node(int(example_list.popleft()))
+#         if cur_node.data <= new_node.data:
+#             while True:
+#                 if cur_node.next_node:
+#                     tmp = cur_node.next_node
+#                     if new_node.data <= tmp.data:
+#                         linked_list.insert(cur_node, new_node)
+#                         break
+#                     else:
+#                         cur_node = tmp
+#                 else:
+#                     linked_list.insert(cur_node, new_node)
+#                     break
+#         else:
+#             linked_list.first_insert(new_node)
+#
+#     linked_list.show()
+
 
 def test_number_10816():
     # 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다.
@@ -100,3 +142,21 @@ def test_number_10816():
     m_list = [0 for i in range(int(example_list.popleft()))]
     m_list = example_list.popleft().split(' ')
     print(' '.join(map(lambda x: str(n_dict[x]) if x in n_dict else '0', m_list)))
+
+
+def test_number_11650():
+    # 2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+    # 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다.
+    # (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+
+    # 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+
+    example_list = deque(['5', '3 4', '1 1', '1 -1', '2 2', '3 3'])
+
+    tuple_list = []
+    for i in range(int(example_list.popleft())):
+        tuple_list.append(tuple(map(int, example_list.popleft().split(' '))))
+
+    for i in sorted(tuple_list, key=lambda x: (x[0], x[1])):
+        print(i[0], i[1])
