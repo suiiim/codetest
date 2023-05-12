9835650b36f4e874845482f6cbc87009699697fe
suimha <suim9770@gmail.com>
2023-05-13 00:23:28 +0900

    [programmers/84021] (python) 퍼즐 조각 채우기

--- programmers/level3/puzzle_pieces.py
+++ programmers/level3/puzzle_pieces.py
@@ -1,18 +1,17 @@
-from collections import deque
+from collections import Counter
 
 
 def solution(game_board: list, table: list):
-    answer = -1
+    answer = 0
 
-    # table 블록추출 (dfs, bfs사용)
     def block_explore(board, blank, fill):
         total_block = []
         block = []
-        for row in range(6):
-            for col in range(6):
+        for row in range(len(board)):
+            for col in range(len(board)):
                 if board[row][col] == blank:
                     block.append((row, col))
-                if block and (board[row][col] == fill or col == 5):
+                if block and (board[row][col] == fill or col == len(board) - 1):
                     for b in total_block:
                         for i in map(lambda x: x[1], block):
                             if (row - 1, i) in b:
@@ -23,30 +22,26 @@ def solution(game_board: list, table: list):
                         block = []
         return total_block
 
+    def compare_block(a, b):
+        for _ in range(4):
+            a = list(map(lambda x: (x[0] - min([i[0] for i in a]), x[1] - min([i[1] for i in a])), a))
+            b = list(map(lambda x: (x[0] - min([i[0] for i in b]), x[1] - min([i[1] for i in b])), b))
+            if sorted(a) == sorted(b):
+                return True
+            else:
+                row = max(max([i[0] for i in a]), max([i[1] for i in a]))
+                a = list(map(lambda x: (x[1], x[0] + (row - x[0] * 2)), a))
+        return False
+
     game_block = block_explore(game_board, 0, 1)
     table_block = block_explore(table, 1, 0)
 
-    def minimum_value(ll):
-        ll = list(map(lambda x: (x[0] - min([i[0] for i in ll]), x[1] - min([i[1] for i in ll])), ll))
-        return ll
-
     for t in table_block:
         for g in filter(lambda x: len(x) == len(t), game_block):
-            t = list(map(lambda x: (x[0] - min([i[0] for i in t]), x[1] - min([i[1] for i in t])), t))
-            g = list(map(lambda x: (x[0] - min([i[0] for i in g]), x[1] - min([i[1] for i in g])), g))
-            print()
-
-    print()
-
-    # game_board 빈곳 추출
-
-    # 탐색 함수 구현
-    def dfs():
-        pass
-
-    # 턴함수 구현
-    def block_turn():
-        pass
+            if compare_block(t, g):
+                answer += len(g)
+                game_block.remove(g)
+                break
 
     return answer
 
