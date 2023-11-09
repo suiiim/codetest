715037ca4cb76c125efe6c810b8262118e6b4d08
suim <suim9770@gmail.com>
2023-11-09 11:04:37 +0900

    [programmers/87946] (python) 피로도

--- programmers/python/level2/bruteForce_피로도.py
+++ programmers/python/level2/bruteForce_피로도.py
@@ -16,6 +16,33 @@ def solution(k, dungeons):
     return max(answer1, answer2)
 
 
+solution1 = lambda k, d: max([solution1(k - u, d[:i] + d[i + 1:]) + 1 for i, (m, u) in enumerate(d) if k >= m] or [0])
+
+answer = 0
+N = 0
+visited = []
+
+
+def dfs(k, cnt, dungeons):
+    global answer
+    if cnt > answer:
+        answer = cnt
+
+    for j in range(N):
+        if k >= dungeons[j][0] and not visited[j]:
+            visited[j] = 1
+            dfs(k - dungeons[j][1], cnt + 1, dungeons)
+            visited[j] = 0
+
+
+def solution2(k, dungeons):
+    global N, visited
+    N = len(dungeons)
+    visited = [0] * N
+    dfs(k, 0, dungeons)
+    return answer
+
+
 if __name__ == '__main__':
     print(solution(80, [[80, 20], [50, 40], [30, 10]]))  # 3
     print(solution(8, [[7, 3], [5, 4], [1, 1]]))  # 3
