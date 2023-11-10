bdb174dac234603fbd2e4ffcc2c4a5f573823660
suim <suim9770@gmail.com>
2023-11-10 17:07:59 +0900

    [programmers/42884] (python) 단속카메라

--- programmers/python/level3/greedy_단속카메라.py
+++ programmers/python/level3/greedy_단속카메라.py
@@ -16,5 +16,30 @@ def solution(routes):
     return answer + 1
 
 
+def solution1(routes):
+    routes = sorted(routes, key=lambda x: x[1])
+    last_camera = -30000
+
+    answer = 0
+
+    for route in routes:
+        if last_camera < route[0]:
+            answer += 1
+            last_camera = route[1]
+
+    return answer
+
+
+def solution2(routes):
+    answer = 0
+    routes.sort(key=lambda x: x[0], reverse=True)
+    camera = 30001
+    for route in routes:
+        if camera > route[1]:
+            answer += 1
+            camera = route[0]
+    return answer
+
+
 if __name__ == '__main__':
     print(solution([[-20, -15], [-14, -5], [-18, -13], [-5, -3]]))  # 2
