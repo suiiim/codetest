51dac99c34c37d033de95a1be6a2cafba856fe87
hattuping <suim9770@gmail.com>
2025-12-22 20:20:35 +0900

    [leetcode] (python) Coin Change 2

---
+++ leetcode/python/daily_question/coin_change_2.py
@@ -0,0 +1,35 @@
+from typing import List
+
+"""
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+You may assume that you have an infinite number of each kind of coin. The answer is guaranteed to fit into a signed 32-bit integer.
+"""
+
+
+class Solution:
+    # Runtime 263 ms -> 63.55%
+    # Memory 17.48 MB -> 99.52%
+    def change(self, amount: int, coins: List[int]) -> int:
+        if amount == 0:
+            return 1
+
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in sorted(coins):
+            for i in range(1, amount + 1):
+                if i < coin:
+                    pass
+                elif i == coin:
+                    dp[i] += 1
+                else:
+                    dp[i] += dp[i - coin]
+
+        return dp[amount]
+
+
+a = Solution()
+print(a.change(amount=5, coins=[5, 2, 1]))  # 4
+print(a.change(amount=3, coins=[2]))  # 0
+print(a.change(amount=10, coins=[10]))  # 1
+print(a.change(amount=500, coins=[1, 2, 5]))  # 12701
